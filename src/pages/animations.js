c439f6a398c291cd38065d7183980d1a8640d0ed
Steve <hail@satan.com>
2020-03-13 05:19:01 -0700

    Gallery class creation for sql injection

--- src/pages/animations.js
+++ src/pages/animations.js
@@ -3,16 +3,18 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Gallery from "../components/gallery"
 
-
-const Animations = () => (
-  <Layout>
-    <SEO title="Animations" />
-
-
-
-    <Link to="/">Home</Link>
-  </Layout>
-)
+class Animations extends React.Component {
+  render() {
+  
+  return(
+    <Layout>
+      <SEO title="Animations" />
+      <Gallery context={this.context} />
+      <Link to="/">Home</Link>
+    </Layout>
+)}
+}
 
 export default Animations
